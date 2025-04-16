4619bae6f26293049546b285211308f7a37e72d0
arash-jj <arshjfry66@gmail.com>
2025-04-16 19:25:55 +0330

    basic UI added

---
+++ games/Flappy-Bird/script.js
@@ -0,0 +1,82 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let frame = 0;
+
+// Load images
+const background = new Image();
+background.src = "../../assets/FlappyBird/images/background-day.png";
+
+const ground = new Image();
+ground.src = "../../assets/FlappyBird/images/base.png";
+
+// Bird animation frames
+const yellowBirdFrames = [
+    "../../assets/FlappyBird/images/yellowbird-upflap.png",
+    "../../assets/FlappyBird/images/yellowbird-midflap.png",
+    "../../assets/FlappyBird/images/yellowbird-downflap.png",
+    "../../assets/FlappyBird/images/yellowbird-midflap.png",
+];
+const birdImages = yellowBirdFrames.map(src => {
+    const img = new Image();
+    img.src = src;
+    return img;
+});
+
+// Background
+const bg = {
+    sX: 0, sY: 0,
+    w: 288, h: 512,
+    x: 0,
+    y: canvas.height - 512,
+    draw: function () {
+        ctx.drawImage(background, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(background, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+};
+
+// Ground
+const base = {
+    sX: 0, sY: 0,
+    w: 336, h: 112,
+    x: 0,
+    y: canvas.height - 112,
+    draw: function () {
+        ctx.drawImage(ground, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+};
+
+// Bird
+const bird = {
+    animation: birdImages,
+    w: 34, h: 24,
+    x: 50, y: 150,
+    animationIndex: 0,
+    draw: function () {
+        const currentFrame = this.animation[this.animationIndex];
+        ctx.drawImage(currentFrame, this.x, this.y, this.w, this.h);
+    }
+};
+
+function update() {
+    // if (frame % 10 === 0) {
+    //     bird.animationIndex = (bird.animationIndex + 1) % bird.animation.length;
+    // }
+}
+
+function draw() {
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    bg.draw();
+    base.draw();
+    bird.draw();
+}
+
+function animate() {
+    update();
+    draw();
+    frame++;
+    requestAnimationFrame(animate);
+}
+
+animate();
