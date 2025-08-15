62936ea80d85c95d0f106f9f40b7db71b56dc882
arash-jj <arshjfry66@gmail.com>
2025-08-15 15:48:34 +0330

    (memory cards game): dynamic images added to UI

--- projects/Memory -Card-Game/script.js
+++ projects/Memory -Card-Game/script.js
@@ -1,16 +1,45 @@
-// images array
+const cardsContainer = document.querySelector('.cards')
+const cards = document.querySelectorAll('.card')
 const images = ["💐","🌹","🌻","🏵️","🌺","🌴","🌈","🍓","🍒","🍎","🍉","🍊","🥭","🍍","🍋","🍏","🍐","🥝","🍇","🥥","🍅","🌶️","🍄","🧅","🥦","🥑","🍔","🍕","🧁","🎂","🍬","🍩","🍫","🎈"];
 
 // get random amount of the array
 const randomNumGenerator = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min)
 }
-let randomNum = randomNumGenerator(1, 26)
+const randomNum = randomNumGenerator(1, 26)
 const selectedImages = images.slice(randomNum, randomNum + 8)
+
+// putting the images in the UI
+const cardGenerator = () => {
+    let cards = ''
+    selectedImages.map(image => {
+        cards += `
+        <div class="card">
+            <div class="back">${image}</div>
+            <div class="front">?</div>
+        </div>
+        `
+    })
+    cardsContainer.innerHTML = cards
+    const shuffled = selectedImages.toSorted(() => Math.random() - 0.5);
+    let shuffledCard = ''
+    shuffled.map(image => {
+        shuffledCard += `
+        <div class="card">
+            <div class="back">${image}</div>
+            <div class="front">?</div>
+        </div>
+        `
+    })
+    cardsContainer.innerHTML += shuffledCard
+}
+cardGenerator()
 
 // ! test
 document.querySelectorAll(".card").forEach(elem=>{
     elem.addEventListener("click",()=>{
-        elem.classList.toggle("flip")
+        elem.classList.add("flip")
+        console.log(flippedCards);
+        
     })
 })
